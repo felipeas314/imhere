66621706f2b1c5790b3ca2e655ce51aec1a5dd96
Felipe Alexandre <felipeas314@gmail.com>
2024-03-19 11:01:17 -0300

    feat: first commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native"
+
+export default function App() {
+  return (
+    <View>
+       <Text>Hello</Text>
+       <Text>Hello 2</Text>
+       <Text>Hello 3</Text>
+    </View>
+  )
+}
