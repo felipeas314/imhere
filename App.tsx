442586fe26fc206ae83dc314a1a6d20ebec0321c
Felipe Alexandre <felipeas314@gmail.com>
2024-03-19 11:26:40 -0300

    feat: separando em componentes

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
-import { Text, View } from "react-native"
+import { StatusBar } from "react-native";
+
+import { Home } from "./src/screens/Home";
 
 export default function App() {
   return (
-    <View>
-       <Text>Hello</Text>
-       <Text>Hello 2</Text>
-       <Text>Hello 3</Text>
-    </View>
+    <>
+      <StatusBar 
+        barStyle="light-content" 
+        backgroundColor="transparent" 
+        translucent
+      />
+      <Home />
+    </>
   )
 }
